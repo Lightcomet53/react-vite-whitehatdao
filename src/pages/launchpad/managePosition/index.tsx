3459447c57337d91ef740285df7a7a50a19e3dbe
dragonfly313 <dragonfly04210@gmail.com>
2024-11-20 20:33:23 -0600

    fix: fixed trading chart remove bug when navigating to another page

--- src/pages/launchpad/managePosition/index.tsx
+++ src/pages/launchpad/managePosition/index.tsx
@@ -1,18 +1,18 @@
-import BitRivals from "@/pages/launchpad/BitRivals";
-import LeftSidebar from "./LeftSideBar";
-import RightSidebar from "./RightSideBar";
-import Transactions from "./Transactions";
-import TradingViewWidget from "@/components/trading-view";
+import BitRivals from '@/pages/launchpad/BitRivals'
+import LeftSidebar from './LeftSideBar'
+import RightSidebar from './RightSideBar'
+import Transactions from './Transactions'
+import TradingViewWidget from '@/components/trading-view'
 
 const ManagePosition: React.FC = () => {
   return (
     <>
       <div className='flex 2xl:h-[calc(100vh-85px)] xl:h-[calc(100vh-83px)] sm:h-[calc(100vh-80px)] h-[calc(100vh-63px)] justify-between'>
         <LeftSidebar />
-        <div className='pt-3 px-4 w-[60vw] lg:w-[66vw] border-borderColor border-r '>
+        <div className='pt-3 px-4 w-[60vw] lg:w-[66vw] border-borderColor border-r'>
           <BitRivals />
-          <div className="h-[calc(100vh-177px)] overflow-y-scroll -mx-4">
-            <div className="h-[500px] mb-4">
+          <div className='h-[calc(100vh-177px)] overflow-y-scroll -mx-4'>
+            <div className='h-[500px] mb-4'>
               <TradingViewWidget />
             </div>
             <Transactions />
--- src/pages/launchpad/presaleDetail/index.tsx
+++ src/pages/launchpad/presaleDetail/index.tsx
@@ -2,16 +2,19 @@ import BitRivals from '@/pages/launchpad/BitRivals'
 import LeftSidebar from './LeftSideBar'
 import RightSidebar from './RightSideBar'
 import Transactions from './Transactions'
+import TradingViewWidget from '@/components/trading-view'
 
 const PresaleDetail: React.FC = () => {
   return (
     <>
       <div className='flex 2xl:h-[calc(100vh-73px)] xl:h-[calc(100vh-69px)] sm:h-[calc(100vh-65px)] h-[calc(100vh-63px)] justify-between'>
         <LeftSidebar />
-        <div className='pt-3 px-4 w-[60vw] lg:w-[66vw]  border-borderColor border-r'>
+        <div className='pt-3 px-4 w-[60vw] lg:w-[66vw] border-borderColor border-r'>
           <BitRivals />
-          <div className='h-[calc(100vh-176px)] -mx-4'>
-            <div className='h-5/6'></div>
+          <div className='h-[calc(100vh-177px)] overflow-y-scroll -mx-4'>
+            <div className='h-[500px] mb-4'>
+              <TradingViewWidget />
+            </div>
             <Transactions />
           </div>
         </div>
